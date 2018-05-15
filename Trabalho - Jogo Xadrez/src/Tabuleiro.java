ad288119cb8e4f466a3b7873cac08a3258ed032f
Bruno Pedrazza <pedrazzabruno@gmail.com>
2018-05-15 18:46:33 -0300

    Trocando a imagem das peças e aumentando

--- Trabalho - Jogo Xadrez/src/Tabuleiro.java
+++ Trabalho - Jogo Xadrez/src/Tabuleiro.java
@@ -34,59 +34,71 @@ public class Tabuleiro extends JComponent {
 					case 0:{
 						if(j==0 || j==7) {
 							posicoes[i][j].setTipo(tipoPeça.Torre);
-							posicoes[i][j].setImg("imagens/Pecas_1/p_torre.gif");
+							//posicoes[i][j].setImg("imagens/Pecas_1/p_torre.gif");
+							posicoes[i][j].setImg("imagens/Pecas_2/PurpleR.png");
 						}
 						else if(j==1 || j==6) {
 							posicoes[i][j].setTipo(tipoPeça.Cavalo);
-							posicoes[i][j].setImg("imagens/Pecas_1/p_cavalo.gif");
+							//posicoes[i][j].setImg("imagens/Pecas_1/p_cavalo.gif");
+							posicoes[i][j].setImg("imagens/Pecas_2/PurpleN.png");
 						}
 						else if(j==2 || j==5) {
 							posicoes[i][j].setTipo(tipoPeça.Bispo);
-							posicoes[i][j].setImg("imagens/Pecas_1/p_bispo.gif");
+							//posicoes[i][j].setImg("imagens/Pecas_1/p_bispo.gif");
+							posicoes[i][j].setImg("imagens/Pecas_2/PurpleB.png");
 						}
 						else if(j==3) {
 							posicoes[i][j].setTipo(tipoPeça.Rainha);
-							posicoes[i][j].setImg("imagens/Pecas_1/p_dama.gif");
+							//posicoes[i][j].setImg("imagens/Pecas_1/p_dama.gif");
+							posicoes[i][j].setImg("imagens/Pecas_2/PurpleQ.png");
 						}
 						else {
 							posicoes[i][j].setTipo(tipoPeça.Rei);
-							posicoes[i][j].setImg("imagens/Pecas_1/p_rei.gif");
+							//posicoes[i][j].setImg("imagens/Pecas_1/p_rei.gif");
+							posicoes[i][j].setImg("imagens/Pecas_2/PurpleK.png");
 						}
 						posicoes[i][j].setCor(1);
 						break;
 					}
 					case 1:{
 						posicoes[i][j].setTipo(tipoPeça.Peao);
-						posicoes[i][j].setImg("imagens/Pecas_1/p_peao.gif");
+						//posicoes[i][j].setImg("imagens/Pecas_1/p_peao.gif");
+						posicoes[i][j].setImg("imagens/Pecas_2/PurpleP.png");
 						posicoes[i][j].setCor(1);
 						break;
 					}
 					case 6:{
 						posicoes[i][j].setTipo(tipoPeça.Peao);
-						posicoes[i][j].setImg("imagens/Pecas_1/b_peao.gif");
+						//posicoes[i][j].setImg("imagens/Pecas_1/b_peao.gif");
+						posicoes[i][j].setImg("imagens/Pecas_2/CyanP.png");
 						posicoes[i][j].setCor(0);
 						break;
 					}
 					case 7:{
 						if(j==0 || j==7) {
 							posicoes[i][j].setTipo(tipoPeça.Torre);
-							posicoes[i][j].setImg("imagens/Pecas_1/b_torre.gif");
+							//posicoes[i][j].setImg("imagens/Pecas_1/b_torre.gif");
+							posicoes[i][j].setImg("imagens/Pecas_2/CyanR.png");
 						}
 						else if(j==1 || j==6) {
 							posicoes[i][j].setTipo(tipoPeça.Cavalo);
-							posicoes[i][j].setImg("imagens/Pecas_1/b_cavalo.gif");
+							//posicoes[i][j].setImg("imagens/Pecas_1/b_cavalo.gif");
+							posicoes[i][j].setImg("imagens/Pecas_2/CyanN.png");
 						}
 						else if(j==2 || j==5) {
 							posicoes[i][j].setTipo(tipoPeça.Bispo);
-							posicoes[i][j].setImg("imagens/Pecas_1/b_bispo.gif");
+							//posicoes[i][j].setImg("imagens/Pecas_1/b_bispo.gif");
+							posicoes[i][j].setImg("imagens/Pecas_2/CyanB.png");
 						}
 						else if(j==3) {
 							posicoes[i][j].setTipo(tipoPeça.Rainha);
-							posicoes[i][j].setImg("imagens/Pecas_1/b_dama.gif");
+							//posicoes[i][j].setImg("imagens/Pecas_1/b_dama.gif");
+							posicoes[i][j].setImg("imagens/Pecas_2/CyanQ.png");
 						}
 						else {
 							posicoes[i][j].setTipo(tipoPeça.Rei);
-							posicoes[i][j].setImg("imagens/Pecas_1/b_rei.gif");
+							//posicoes[i][j].setImg("imagens/Pecas_1/b_rei.gif");
+							posicoes[i][j].setImg("imagens/Pecas_2/CyanK.png");
 						}
 						posicoes[i][j].setCor(0);
 						break;
@@ -121,7 +133,7 @@ public class Tabuleiro extends JComponent {
 				g2d.fill(casas[i][j]);
 				
 				if(posicoes[i][j]!=null) {
-					g2d.drawImage(posicoes[i][j].getImg(), posicaoX+((larguraCasa-50)/2), posicaoY+((alturaCasa-50)/2), 50, 50, null);
+					g2d.drawImage(posicoes[i][j].getImg(), posicaoX+((larguraCasa-65)/2), posicaoY+((alturaCasa-65)/2), 65, 65, null);
 				}
 				
 				if(cor == Color.WHITE)
